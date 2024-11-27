9f10375cce3fd1a66c3035761946a9ba89c370bb
keikaku-unit <188527347+keikaku-unit@users.noreply.github.com>
2024-11-27 15:47:49 +0900

    flatgeobuf追加

--- main.js
+++ main.js
@@ -2,30 +2,116 @@ import './style.css'
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import { Protocol } from "pmtiles";
+import { throttle } from 'underscore';
 
 let protocol = new Protocol();
 maplibregl.addProtocol("pmtiles", protocol.tile);
 
-const map = new maplibregl.Map({
-  container: 'map',
-  hash: true,
-  style: './style.json',
-  center: [135.57, 34.777],
-  zoom: 13,
-  maxZoom: 17.99,
-  minZoom: 4,
-  localIdeographFontFamily: ['BIZ UDPGothic', 'sans-serif']
-});
+const fgbMinZoom = 18;
 
-map.addControl(new maplibregl.NavigationControl(), 'bottom-right');
+document.addEventListener("DOMContentLoaded", async () => {
+  // basic MapLibre map
+  const map = new maplibregl.Map({
+    container: 'map',
+    hash: true,
+    style: './style.json',
+    center: [135.57, 34.777],
+    zoom: 13,
+    maxZoom: 19,
+    minZoom: 4,
+    localIdeographFontFamily: ['BIZ UDPGothic', 'sans-serif']
+  });
 
-let check1 = document.getElementById('style');
-let check2 = document.getElementById('bdr');
+  // convert the rect into the format flatgeobuf expects
+  function fgBoundingBox() {
+    const { _sw, _ne } = map.getBounds();
+    return {
+      minX: _sw.lng,
+      minY: _sw.lat,
+      maxX: _ne.lng,
+      maxY: _ne.lat,
+    };
+  }
 
-document.getElementById('style').addEventListener('click', () => {
-  check1.checked ? map.setStyle('./style/std.json') : map.setStyle('./style/skeleton.json');
-});
+  async function updateResults() {
+    if (map.getZoom() < fgbMinZoom) {
+      return;
+    };
 
-document.getElementById('bdr').addEventListener('click', () => {
-  check2.checked ? map.showTileBoundaries = true : map.showTileBoundaries = false;
+    let i = 0;
+    const fc = { type: "FeatureCollection", features: [] };
+    let iter = flatgeobuf.deserialize("./data/r1chikeizu.fgb", fgBoundingBox());
+    for await (let feature of iter) {
+      fc.features.push({ ...feature, id: i });
+      i += 1;
+    }
+    map.getSource("blocks").setData(fc);
+  }
+
+  map.on("load", () => {
+    map.addSource("blocks", {
+      type: "geojson",
+      data: { type: "FeatureCollection", features: [] },
+    });
+    map.addLayer({
+      "id": "blocks-line",
+      "type": "line",
+      "source": "blocks",
+      "minzoom": fgbMinZoom,
+      "paint": {
+        "line-color": [
+          "match",
+          [
+            "get",
+            "layer"
+          ],
+          "建物",
+          "#232323",
+          "道路",
+          "#232323",
+          "名称",
+          "#a8a1ad",
+          "#7b7b7d"
+        ],
+        "line-width": [
+          "match",
+          [
+            "get",
+            "layer"
+          ],
+          "鉄道",
+          2.6,
+          "行政界等",
+          1.6,
+          "河川",
+          1.6,
+          0.6
+        ],
+      }
+    });
+
+    // if the user is panning around alot, only update twice per second max
+    updateResults = throttle(updateResults, 500);
+
+    // show results based on the initial map
+    updateResults();
+
+    // ...and update the results whenever the map moves
+    map.on("moveend", function (s) {
+      updateResults();
+    });
+  });
+
+  map.addControl(new maplibregl.NavigationControl(), 'bottom-right');
+
+  let check1 = document.getElementById('style');
+  let check2 = document.getElementById('bdr');
+
+  document.getElementById('style').addEventListener('click', () => {
+    check1.checked ? map.setStyle('./style/std.json') : map.setStyle('./style/skeleton.json');
+  });
+
+  document.getElementById('bdr').addEventListener('click', () => {
+    check2.checked ? map.showTileBoundaries = true : map.showTileBoundaries = false;
+  });
 });
--- readme.md
+++ readme.md
@@ -8,6 +8,8 @@ https://settsu-city-planning.github.io/City-Planning-Viewer/
 
 [PMTiles](https://docs.protomaps.com/pmtiles/)
 
+[Underscore.js](https://underscorejs.org/)
+
 [Node.js](https://nodejs.org/en)
 
 [Vite](https://ja.vite.dev/)
@@ -16,9 +18,19 @@ https://settsu-city-planning.github.io/City-Planning-Viewer/
 
 [Git](https://git-scm.com/)
 
+[QGIS](https://www.qgis.org/)
+
 ## 参考文献・サイト
 [現場のプロがわかりやすく教える 位置情報エンジニア養成講座](https://www.shuwasystem.co.jp/book/9784798068923.html)
 
 [最適化ベクトルタイル試験公開](https://github.com/gsi-cyberjapan/optimal_bvmap)
+
+[ベクトルタイル用スタイル・Expressionsについて（MapBoxスタイル仕様、MapLibreスタイル仕様）](https://qiita.com/T-ubu/items/961176fb92fb66a927e0)
+
+[Layers - MapLibre Style Spec](https://maplibre.org/maplibre-style-spec/layers/)
+
+[FlatGeobuf - MapLibre Example - Filtering a Large Dataset](https://flatgeobuf.org/examples/maplibre/large.html)
+
+[MapLibreでFlatGeobuf形式の町丁目ポリゴンを表示してみる](https://qiita.com/y-mto/items/185af00a224f4c9730a0)
 
 [オープンソースソフトウェア（OSS）ライセンスの比較・まとめ、GPLやMITは何が違うのか いま最も利用されているライセンスは？｜ビジネス+IT](https://www.sbbit.jp/article/cont1/34811)
